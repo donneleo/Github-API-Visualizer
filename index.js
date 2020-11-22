599f29b3ea3bed7ed279d80bd4dd8889a2d730c6
Eoin Maguire <60257358+donneleo@users.noreply.github.com>
2020-11-22 17:15:11 +0000

    small update, needs more work.
    Placeholder pie chart in place. Parsing function needs to be looked at

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+
 function handleInput()
 {
   var x = document.getElementById("fname").value;
@@ -16,5 +19,41 @@ async function main(user) {
   
   let url = `https://api.github.com/users/${user}/repos`;
   let reposData = await GetRequest(url).catch(error => console.error(error));
- 
+
+  commitsPerRepo(reposData, user)
 }
+
+
+async function commitsPerRepo(userReposData, user) {
+  let commits = [];
+  for (let i = 0; i < userReposData.length; i++) 
+  {
+    const repo = userReposData[i].name;
+    let a = await GetRequest(`https://api.github.com/repos/${user}/${repo}/commits`).catch((error) => console.error(error));
+    let b = { repo: repo, commits: a.length };
+    commits.push(b);
+  }
+  //drawChart(commits);
+}
+
+function drawChart(){
+  var data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Work',     11],
+    ['Eat',      2],
+    ['Commute',  2],
+    ['Watch TV', 2],
+    ['Sleep',    7]
+  ]);
+
+  var options = {
+    title: 'Commits',
+    is3D: true,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+  chart.draw(data, options);
+};
+
+
+
